17c394fff9f79d208d52662ce2678eb111a304a8
Murali Raju <murali.raju@appliv.com>
2013-07-02 17:19:22 -0400

    Updates

---
+++ cookbooks/bdas/recipes/spark_worker.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: bdas
+# Recipe:: [Setup a Berkeley Data Analysis Stack Environment - Spark Worker]
+#
+# Copyright 2013, Velankani Information Systems, Inc eng@velankani.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# cookbooksributed under the License is cookbooksributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+
+spark_master = node[:bdas][:spark][:master]
+spark_home = node[:bdas][:spark][:home]
+
+
+execute "Starting Spark Worker" do
+  command "#{spark_home}/run spark.deploy.worker.Worker spark://#{spark_master}:7077 &"
+  action :run
+end
