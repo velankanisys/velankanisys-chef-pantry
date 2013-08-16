a01f2c5fcddef303fdc9758c789c4f57631ba4d1
Murali Raju <murali.raju@appliv.com>
2013-08-16 08:07:33 -0400

    oozie-site xml fix for HDP 1.3

--- cookbooks/hadoop-hdp/recipes/flume.rb
+++ cookbooks/hadoop-hdp/recipes/flume.rb
@@ -18,34 +18,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-%w'flume-ng
-flume-ng-agent'.each do | pack |
+%w'flume
+flume-node'.each do | pack |
   package pack do
     action :install
     #options "--force-yes"
   end
 end
 
-remote_file "/usr/lib/flume-ng/lib/flume-sources-1.0-SNAPSHOT.jar" do
+remote_file "/usr/lib/flume/lib/flume-sources-1.0-SNAPSHOT.jar" do
   source "http://files.cloudera.com/samples/flume-sources-1.0-SNAPSHOT.jar"
-  not_if { File.exists?("/usr/lib/flume-ng/lib/flume-sources-1.0-SNAPSHOT.jar") }
+  not_if { File.exists?("/usr/lib/flume/lib/flume-sources-1.0-SNAPSHOT.jar") }
 end
 
-template "/etc/flume-ng/conf/flume-env.sh" do
+template "/etc/flume/conf/flume-env.sh" do
   source "flume-env.sh.erb"
   owner "root"
   group "root"
   mode 0755
 end
 
-template "/etc/default/flume-ng-agent" do
-  source "flume-ng-agent.erb"
-  owner "root"
-  group "root"
-  mode 0755
-end
+# template "/etc/default/flume-ng-agent" do
+#   source "flume-ng-agent.erb"
+#   owner "root"
+#   group "root"
+#   mode 0755
+# end
 
-template "/etc/flume-ng/conf/flume.conf" do
+template "/etc/flume/conf/flume.conf" do
   source "flume.conf.erb"
   owner "root"
   group "root"
