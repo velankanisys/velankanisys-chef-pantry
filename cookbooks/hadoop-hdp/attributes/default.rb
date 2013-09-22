ed696fec6c860e6aed674b2aed514c6130e7f83b
Murali Raju <murali.raju@appliv.com>
2013-09-21 20:22:31 -0400

    updates

--- cookbooks/hadoop-hdp/attributes/default.rb
+++ cookbooks/hadoop-hdp/attributes/default.rb
@@ -1,25 +1,30 @@
 default[:hortonworks_hdp][:manage_all_config_files] = false
 default[:hortonworks_hdp][:nofiles] = 32768
-default[:hortonworks_hdp][:swapfile_location] = "/media/ephemeral0/swapfile"
-#default[:hortonworks_hdp][:swapfile_location] = "/swapfile"
+#default[:hortonworks_hdp][:swapfile_location] = "/media/ephemeral0/swapfile"
+default[:hortonworks_hdp][:swapfile_location] = "/mnt/swapfile"
 
 default[:hortonworks_hdp][:namenode][:host] = "hadoop-hdp-node1"
 default[:hortonworks_hdp][:namenode][:port] = "8020"
 default[:hortonworks_hdp][:namenode][:safemode_min_datanodes] = 3
 default[:hortonworks_hdp][:namenode][:num_dfs_replicas] = 3
-default[:hortonworks_hdp][:namenode][:dfs_name_dir] = "/media/ephemeral0/var/lib/hadoop/cache/hadoop/dfs/name" 
-default[:hortonworks_hdp][:namenode][:hadoop_tmp_dir] = "/media/ephemeral0/var/lib/hadoop/cache/${user.name}"
+# default[:hortonworks_hdp][:namenode][:dfs_name_dir] = "/media/ephemeral0/var/lib/hadoop/cache/hadoop/dfs/name" 
+# default[:hortonworks_hdp][:namenode][:hadoop_tmp_dir] = "/media/ephemeral0/var/lib/hadoop/cache/${user.name}"
+# default[:hortonworks_hdp][:namenode][:dfs_name_dir_root] = ""
+default[:hortonworks_hdp][:namenode][:dfs_name_dir] = "/mnt/var/lib/hadoop/cache/hadoop/dfs/name" 
+default[:hortonworks_hdp][:namenode][:hadoop_tmp_dir] = "/mnt/var/lib/hadoop/cache/${user.name}"
 default[:hortonworks_hdp][:namenode][:dfs_name_dir_root] = ""
+
 
 # default[:hortonworks_hdp][:namenode][:dfs_name_dir] = "/var/lib/hadoop/cache/hadoop/dfs/name" 
 # default[:hortonworks_hdp][:namenode][:hadoop_tmp_dir] = "/var/lib/hadoop/cache/${user.name}"
 # default[:hortonworks_hdp][:namenode][:dfs_name_dir_root] = "/hadoop"
 
 #DataNode
-default[:hortonworks_hdp][:datanode][:dfs_data_dir] = "/media/ephemeral0/var/lib/hadoop/cache/hdfs/dfs/data"  
+#default[:hortonworks_hdp][:datanode][:dfs_data_dir] = "/media/ephemeral0/var/lib/hadoop/cache/hdfs/dfs/data"
+default[:hortonworks_hdp][:datanode][:dfs_data_dir] = "/mnt/var/lib/hadoop/cache/hdfs/dfs/data"   
 #default[:hortonworks_hdp][:datanode][:dfs_data_dir] = "/hadoop/var/lib/hadoop/cache/hdfs/dfs/data"
 
-#Map Reduce
+#MapReduce
 default[:hortonworks_hdp][:jobtracker][:host] = "hadoop-hdp-node2"
 default[:hortonworks_hdp][:jobtracker][:port] = "8021"
 # default[:hortonworks_hdp][:mapreduce][:mapred_child_java_opts] = "-server -Xmx2048m -Djava.net.preferIPv4Stack=true"
